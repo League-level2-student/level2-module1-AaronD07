c5b42487c4a6a2debbd54e80486f5b986917ead2
League Student <league@iMac-27.attlocal.net>
2020-01-02 18:42:10 -0800

    :smile:

--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -1,14 +1,34 @@
 package intro_to_array_lists;
 
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList class
-		
+		ArrayList<String> list = new ArrayList<String>();
 		//2. Add five Strings to your list
+		list.add("1");
+		list.add("2");
+		list.add("3");
+		list.add("4");
+		list.add("5");
 		
 		//3. Print all the Strings using a standard for-loop
-		
+		for(int i=0; i<10; i++) {
+			JOptionPane.showMessageDialog(null, list);
+			if (i%2==0) {
+				JOptionPane.showMessageDialog(null, list);
+			}
+			JOptionPane.showMessageDialog(null, list);
+			if (list.contains("e")) {
+				
+			
+			JOptionPane.showMessageDialog(null, list);
+		}
+		}
 		//4. Print all the Strings using a for-each loop
 		
 		//5. Print only the even numbered elements in the list.
